9449a86df90510171a9722b3d55db10d1010fffc
WesleyTrescott <wesley.trescott@gmail.com>
2016-04-29 10:49:00 -0400

    update test debug structure

--- ComplexNumbersTest/Debug Gcov/Complex Numbers/ComplexNumber.d
+++
@@ -1,8 +0,0 @@
-Complex\ Numbers/ComplexNumber.d: \
- /home/wesley/workspace/Complex\ Numbers/ComplexNumber.cpp \
- /home/wesley/workspace/Complex\ Numbers/ComplexNumber.h \
- /home/wesley/workspace/Complex\ Numbers/ComplexNumber.h
-
-/home/wesley/workspace/Complex\ Numbers/ComplexNumber.h:
-
-/home/wesley/workspace/Complex\ Numbers/ComplexNumber.h:
--- ComplexNumbersTest/Debug/subdir.mk
+++ ComplexNumbersTest/Debug/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/home/wesley/workspace/ComplexNumbersProject/ComplexNumbersTest/cute" -I"/home/wesley/workspace/ComplexNumbersProject/ComplexNumbersTest/boost" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I"/home/wesley/workspace/ComplexNumbersProject/ComplexNumbersTest/cute" -I"/home/wesley/workspace/ComplexNumbersProject/ComplexNumbersTest/boost" -O0 -g3 -ftest-coverage -fprofile-arcs -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
